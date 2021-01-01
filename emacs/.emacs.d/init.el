9a5ae06f51c0d5a12ade74239819b41ea9d917d1
bandithedoge <bandithedoge@protonmail.com>
2021-01-01 12:20:08 +0100

    fix elpa and remove pywal

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,8 +3,8 @@
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org". "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
