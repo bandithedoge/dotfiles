dd9e8d2a324dd4901cff4dc57d4f691c44c18dd6
bandithedoge <bandithedoge@protonmail.com>
2021-01-04 16:29:30 +0100

    fix sxhkd

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,11 +6,20 @@ set TERM "xterm-256color"
 fish_vi_key_bindings
 
 set fish_color_normal           '#d9dceb'
-set fish_color_autosuggestion   '#737998'
 set fish_color_command          '#69d26e'
-set fish_color_error            '#cf4e54'
-set fish_color_param            '#d9dceb'
 set fish_color_quote            '#d7953f'
+set fish_color_redirection      '#b96be1'
+set fish_color_end              '#e1c85c'
+set fish_color_error            '#eb585f'
+set fish_color_param            '#d9dceb'
+set fish_color_comment          '#474dab'
+
+set fish_color_autosuggestion   '#474dab'
+
+set fish_cursor_default     block
+set fish_cursor_insert      line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual      block
 
 bind f '__fzf_search_current_dir'
 bind u '__fzf_search_history'
