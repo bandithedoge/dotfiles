bce36a24a4945fb7f542807761724d10b1249509
bandithedoge <bandithedoge@protonmail.com>
2023-05-03 14:08:14 +0200

    some windows things

--- windows/wezterm.lua
+++ windows/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require "wezterm"
 
 return {
     default_prog = {"powershell"},
-    color_scheme = "Gruvbox Dark",
+    color_scheme = "GruvboxDarkHard",
     font = wezterm.font "JetBrainsMono NF",
     font_size = 13,
     tab_bar_at_bottom = true,
